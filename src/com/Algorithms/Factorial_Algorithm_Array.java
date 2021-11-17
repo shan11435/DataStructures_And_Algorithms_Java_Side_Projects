14e7cc54477c549f08cb9cd389fe3c141d6cdddf
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-11-17 17:30:50 -0500

    added a new java class

---
+++ src/com/Algorithms/Factorial_Algorithm_Array.java
@@ -0,0 +1,28 @@
+package com.Algorithms;
+
+public class Factorial_Algorithm_Array {
+
+    public static void main(String[] args) {
+        //this will create an array with a size of 7 (only put 7 elements)
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //initialize the sum variable to 0
+        int product = 1;
+        //loops through the array
+        for(int i = array.length - 1; i < array.length; i--)
+        {
+            //if the next iteration is -1 you break out of the loop
+            if(i == -1)
+            {
+                break;
+            }
+            //but if it doesn't equal -1 continue with the loop
+            else
+            {
+                //product is multiplied with each element
+                product = product * array[i];
+            }
+
+        }
+        System.out.println(product);
+    }
+}
